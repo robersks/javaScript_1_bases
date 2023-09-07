7caba56f71821924a12fb4737fe6dd8ec3e4d3fa
Rober Fabian <robersks.s@gmail.com>
2023-09-07 13:29:52 -0500

    Primer Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var max_busqueda = 10
+
+function f_buscar_primos(max_busqueda) {
+    for (let index = 0; index <= max_busqueda; index++) { // index de 0 a 100
+
+    }
+}
+
+f_buscar_primos(max_busqueda);
+
+
+let primos = [];
+
+console.log(primos);
+
+primos.push(50);
+console.log(primos);
