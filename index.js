ad26b09a0ff131a91c79b6d6c5d1fd5f3c89fd63
Rober Fabian <robersks.s@gmail.com>
2023-09-07 23:12:29 -0500

    avance x
    se avanza en el algoritmo del reto

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var max_busqueda = 10
 
 function f_buscar_primos(max_busqueda) {
-    for (let index = 0; index <= max_busqueda; index++) { // index de 0 a 100
+    let primos = [];
+    let primos_temp = [];
+    let primos_gemelos = [];
 
+    primos.push()
+    for (let i = 2; i <= max_busqueda; i++) { // index de 0 a 100  ____ numero que se validará si es primo o no
+
+        let cont = 1
+        for (let i2 =1;i2 <= i; i2++){ // verificar cuantas veces es posible dividir el numero i
+            // console.log(i,i2)
+            console.log(cont);
+            if (i%i2 == 0 || i%i2 == 1 ){ // si i es divisible y el r
+                cont++
+            }
+        }
+
+        if (cont == 2){
+            primos_temp.push(i)
+        }
     }
+    
+    console.log(primos);
+    console.log(primos_temp);
+    console.log(primos_gemelos);
 }
 
-f_buscar_primos(max_busqueda);
+var x = f_buscar_primos(max_busqueda);
+console.log(x)
 
 
-let primos = [];
-
-console.log(primos);
-
-primos.push(50);
-console.log(primos);
